33f70aac4b1d124b0a00e5e8803f14a29b37d9c2
Paul Clarkin <paul.clarkin@laterooms.com>
2016-04-21 10:33:41 +0100

    Test 3

---
+++ C-Sharp/tests/FerryLegacy.Tests/SaffSqueeze.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FerryLegacy.Tests
+{
+
+    [TestFixture]
+    public class SaffSqueeze
+    {
+        [Test]
+        public void test_one()
+        {
+            var timeTables = new TimeTables();
+            var ferries = new Ferries();
+            var bookings = new Bookings();
+            var _ports = new Ports();
+            var _ferryService = new FerryAvailabilityService(_ports, ferries, timeTables, new PortManager(_ports, ferries));
+            var _bookingService = new JourneyBookingService(timeTables, bookings, _ferryService);
+            var _timeTableService = new TimeTableService(timeTables, bookings, _ferryService);
+
+            var ports = _ports.All();
+            var timeTable = _timeTableService.GetTimeTable(ports);
+
+            foreach (var port in ports)
+            {
+                var items = timeTable.Where(x => x.OriginPort == port.Name).OrderBy(x => x.StartTime);
+                var firstBoat = items.First();
+
+                Assert.That(firstBoat.FerryName, Is.EqualTo("Titanic"));
+            }
+        }
+
+        [Test]
+        public void test_two()
+        {
+            var timeTables = new TimeTables();
+            var ferries = new Ferries();
+            var portService = new Ports();
+            var ferryService = new FerryAvailabilityService(portService, ferries, timeTables, new PortManager(portService, ferries));
+
+            var ports = portService.All();
+            var timetables = timeTables.All();
+
+            var allEntries = timetables.SelectMany(x => x.Entries).OrderBy(x => x.Time).ToList();
+            var rows = new List<TimeTableViewModelRow>();
+
+            foreach (var timetable in allEntries)
+            {
+                var origin = ports.Single(x => x.Id == timetable.OriginId);
+                var destination = ports.Single(x => x.Id == timetable.DestinationId);
+                var destinationName = destination.Name;
+                var originName = origin.Name;
+                var ferry = ferryService.NextFerryAvailableFrom(origin.Id, timetable.Time);
+                var arrivalTime = timetable.Time.Add(timetable.JourneyTime);
+                var row = new TimeTableViewModelRow
+                {
+                    DestinationPort = destinationName,
+                    FerryName = ferry == null ? "" : ferry.Name,
+                    JourneyLength = timetable.JourneyTime.ToString("hh':'mm"),
+                    OriginPort = originName,
+                    StartTime = timetable.Time.ToString("hh':'mm"),
+                    ArrivalTime = arrivalTime.ToString("hh':'mm"),
+                };
+                rows.Add(row);
+            }
+
+            var firstRow = rows.First();
+            Assert.That(firstRow.FerryName, Is.EqualTo("Titanic"));
+        }
+
+        [Test]
+        public void test_three()
+        {
+            var timeTables = new TimeTables();
+            var ferries = new Ferries();
+            var portService = new Ports();
+            var ferryService = new FerryAvailabilityService(portService, ferries, timeTables, new PortManager(portService, ferries));
+
+            var ports = portService.All();
+            var timetables = timeTables.All();
+
+            var allEntries = timetables.SelectMany(x => x.Entries).OrderBy(x => x.Time).ToList();
+            var firstEntry = allEntries.First();
+            var origin = ports.Single(x => x.Id == firstEntry.OriginId);
+            var ferry = ferryService.NextFerryAvailableFrom(origin.Id, firstEntry.Time);
+            
+            var theFerryName = ferry == null ? "" : ferry.Name;
+   
+            Assert.That(theFerryName, Is.EqualTo("Titanic"));
+        }
+    }
+}
